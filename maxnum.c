59bf5cfbb1edf8446ef25f6620f3d4d40641420c
Swetaa Sarangi <52197354+swetaa10@users.noreply.github.com>
2021-01-14 15:50:00 +0530

    Create maxnum.c

---
+++ maxnum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int max_of_four(int a, int b, int c, int d)
+{
+    int arr[4]={a,b,c,d};
+    int max=arr[0];
+    for(int i=0;i<4;i++)
+    {
+        if(max<arr[i])
+            max=arr[i];
+    }
+    return max;
+}
+
+int main() 
+{
+    int a, b, c, d;
+    printf("Enter The Numbers:")
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("\nThe Greatest Number is :%d", ans);
+    
+    return 0;
+}
